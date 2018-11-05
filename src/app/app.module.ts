9d87cc353bba011057e1bc93824a45393098fc16
Roger Srey <rsrey@fullmetal.central.com>
2018-11-05 16:58:42 +0100

    Add the first service empty; added to app.module so added int the providers array.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppareilComponent } from './appareil/appareil.component';
 import { FormsModule } from '@angular/forms';
+import { AppareilService} deom './services/appareil.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+  	AppareilService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
