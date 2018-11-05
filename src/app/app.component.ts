ac2ccbfc59014f15aff939dce25cb6645040c6ba
Roger Srey <rsrey@fullmetal.central.com>
2018-11-05 16:25:59 +0100

    Added directive of structure , an ngif and ngFor also change on app ts the name to a array of object appareil.name and appareil.status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'openclassroom-first-project';
   isAuth = false
-  
-  appareilOne = 'Machine à laver';
-  appareilTwo = 'Frigo';
-  appareilThree = 'Ordinateur';
 
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
 
   constructor() {
     setTimeout(
