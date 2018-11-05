50b451603fa392ae82246ba143e23ac5668f81c3
Roger Srey <rsrey@fullmetal.central.com>
2018-11-05 16:44:02 +0100

    Added the first pipe used, pip take input transform and send a result, pipe can be chained.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'openclassroom-first-project';
-  isAuth = false
+  isAuth = false;
+  lastUpdate = new Date();
 
   appareils = [
     {
