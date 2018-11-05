aebf185791393a7e343b8bd8b367e09369468e67
Roger Srey <rsrey@fullmetal.central.com>
2018-11-05 16:39:04 +0100

    Added ngStyle and ngClass directive by attributes that will change the style of the text and the class of li according to status

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -19,4 +19,12 @@ export class AppareilComponent implements OnInit {
   getStatus() {
     return this.appareilStatus;
   }
+
+  getColor() {
+  	if (this.appareilStatus === 'éteint') {
+  		return 'red';
+  	} else if(this.appareilStatus === 'allumé') {
+  		return 'green';
+  	}
+  }
 }
