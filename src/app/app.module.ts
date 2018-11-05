dd0936c0a178fc8a9c749f2876eef9db18045f67
Roger Srey <rsrey@fullmetal.central.com>
2018-11-05 21:23:20 +0100

    Added routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,15 @@ import { FormsModule } from '@angular/forms';
 import { AppareilService } from './services/appareil.service';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import {RouterModule, Routes} from "@angular/router";
+
+
+const appRoutes: Routes = [
+	{ path: 'appareils', component: AppareilViewComponent },
+	{ path: 'auth', component: AuthComponent },
+	{ path: '', component: AppareilViewComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -17,7 +26,8 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
   	AppareilService
