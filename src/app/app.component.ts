67b8010691a3306afe1c1f75b10564a35fa6ee56
Roger Srey <rsrey@fullmetal.central.com>
2018-11-05 16:02:12 +0100

    Added exemple of interpolation, property binding and two binfing through the ngmodule and angularforms.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'openclassroom-first-project';
+  isAuth = false
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+
+    );
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+  }
+
 }
