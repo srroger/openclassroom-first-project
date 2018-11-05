1cbd2a413b36f8dfc44ce79b97e843ef99769769
Roger Srey <rsrey@fullmetal.central.com>
2018-11-05 23:01:09 +0100

    Added a Auth Guard on appareils 'page'.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,15 @@ import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import { AuthService } from './services/auth.service';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { AuthGuard } from './services/auth-guard.service';
 
 import {RouterModule, Routes} from "@angular/router";
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
 
 const appRoutes: Routes = [
-	{ path: 'appareils', component: AppareilViewComponent },
-	{ path: 'appareils/:id', component: SingleAppareilComponent },
+	{ path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
+	{ path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent },
 	{ path: 'auth', component: AuthComponent },
 	{ path: '', component: AppareilViewComponent },
 	{ path: 'not-found', component: FourOhFourComponent },
@@ -40,7 +41,8 @@ const appRoutes: Routes = [
   ],
   providers: [
   	AppareilService,
-  	AuthService
+  	AuthService,
+  	AuthGuard
   ],
   bootstrap: [AppComponent]
 })
