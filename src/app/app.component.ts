829e10eb11ef6dbea4eac1260cafd294f7ecf611
Roger Srey <rsrey@fullmetal.central.com>
2018-11-06 16:01:24 +0100

    Added a Subscription object and a unsubscribe action on the ngOnDestroy to avoid infinite behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AppareilService } from './services/appareil.service';
 import { Observable } from 'rxjs';
 import { interval } from 'rxjs';
+import {Subscription} from 'rxjs';
 
 
 @Component({
@@ -10,25 +11,24 @@ import { interval } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, OnDestroy {
 
 	secondes: number;
+	counterSubscription: Subscription;
 
 	constructor() {}
 
 	ngOnInit()  {
 		const counter = interval(1000);
-		counter.subscribe(
+		this.counterSubscription = counter.subscribe(
 			(value: number) => {
 				this.secondes = value;
-			},
-			(error: any) => {
-				console.log('Une erreur a été rencontrée !');
-			},
-			() => {
-				console.log('Observable complétée !');
 			}
 		);
+	}
+
+	ngOnDestroy() {
+		this.counterSubscription.unsubscribe();
 	}
 
 }
