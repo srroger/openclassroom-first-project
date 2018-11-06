10df10d50e1f1e1cd5b1d0014d88aeb4fcd2815e
Roger Srey <rsrey@fullmetal.central.com>
2018-11-06 15:40:47 +0100

    Added a little Observable counter and observe it through a subscribe function. However since I am on Angular 6 the syntax has changed and I am not using Observable.interval at all, I am using interval() from the 'rxjs'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppareilService } from './services/appareil.service';
+import { Observable } from 'rxjs';
+import { interval } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
 
-	constructor() {
+export class AppComponent implements OnInit{
 
+	secondes: number;
+
+	constructor() {}
+
+	ngOnInit()  {
+		const counter = interval(1000);
+		counter.subscribe(
+			(value: number) => {
+				this.secondes = value;
+			},
+			(error: any) => {
+				console.log('Une erreur a été rencontrée !');
+			},
+			() => {
+				console.log('Observable complétée !');
+			}
+		);
 	}
+
 }
