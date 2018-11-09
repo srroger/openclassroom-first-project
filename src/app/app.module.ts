99b5ac0d1ac82ae652875e1a639dbddb5b020e8f
Roger Srey <rsrey@fullmetal.central.com>
2018-11-09 16:21:11 +0100

    Added a user list system in order to use the reactive method of a formular, for now, there just a new model of user created a new service and a new component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppareilComponent } from './appareil/appareil.component';
@@ -9,17 +10,20 @@ import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import { AuthService } from './services/auth.service';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { UserService } from './services/user.service'
 import { AuthGuard } from './services/auth-guard.service';
 
 import {RouterModule, Routes} from "@angular/router";
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 
 const appRoutes: Routes = [
 	{ path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
 	{ path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent },
   { path: 'edit', canActivate: [AuthGuard], component: EditAppareilComponent },
+  { path: 'users', canActivate: [AuthGuard], component: UserListComponent },
 	{ path: 'auth', component: AuthComponent },
 	{ path: '', component: AppareilViewComponent },
 	{ path: 'not-found', component: FourOhFourComponent },
@@ -35,16 +39,19 @@ const appRoutes: Routes = [
     AppareilViewComponent,
     SingleAppareilComponent,
     FourOhFourComponent,
-    EditAppareilComponent
+    EditAppareilComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
   	AppareilService,
   	AuthService,
+    UserService,
   	AuthGuard
   ],
   bootstrap: [AppComponent]
--- src/app/edit-appareil/edit-appareil.component.ts
+++ src/app/edit-appareil/edit-appareil.component.ts
@@ -1,4 +1,5 @@
-import { Component, , NgForm, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 import { Router } from "@angular/router";
 import { AppareilService } from "../services/appareil.service";
 
